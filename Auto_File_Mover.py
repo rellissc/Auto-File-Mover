fada2cda9a7ea4bf630cc57e5b6a7b5e9f61c2d8
Concrete18 <michaelericson19@gmail.com>
2020-03-05 18:40:33 -0500

    Added more functionality and shorted code using additional functions

--- Auto_File_Mover.py
+++ Auto_File_Mover.py
@@ -4,25 +4,60 @@ import fnmatch
 
 WatchedFolder = 'D:/Downloads'
 
-FileTypeDefaults = {'.mp4': 'Insert Directory Here', '.png': 'Insert Directory Here', '.jpg': 'Insert Directory Here'}
+Settings = {
+    'Monitor Resolution': '2560, 1440'
+}
+
+PathDef = {
+    'Wallpapers': 'D:/Google Drive/Photos/Wallpapers'
+}
+
+FileDef = {
+    '.mp4': 'D:/Downloads/Video',
+    '.png': 'D:/Downloads/Images',
+    '.jpg': 'D:/Downloads/Images',
+    '.exe': 'D:/Downloads/Installers'
+}
+
+
+def FileMove(Target, Dest):
+    shutil.move(WatchedFolder + '/' + Target, Dest)
+    print('Copied to Folder')
+
+
+def FileDelete(Target, Type):
+    print('Found an ' + Type + ' named ' + Target + '.')
+    InstallerDelResp = input('Do you want to delete it?')
+    if InstallerDelResp == 'yes' or InstallerDelResp == 'y':
+        os.remove(WatchedFolder + '/' + Target)
+        print('Deleted File')
+    else:
+        print('Ok, copying ' + Target + 'to the ' + Type + ' default folder.')
+        shutil.move(WatchedFolder + '/' + Target, FileDef[Type])
 
 
 def MoveByName():
     for File in os.listdir(WatchedFolder):
+        # MP4 Checker
         if File.endswith(".mp4"):
             print("MP4 Found")
             if fnmatch.fnmatch(File, '*Test*'):
-                print('Video Found named ' + File)
-                shutil.move(WatchedFolder + '/' + File, "E:/Game install files/Valve/temporary/VR/New")
-                print('Copied to Folder' + File)
+                print('Test Example Found named ' + File)
+                FileMove(File, FileDef['.mp4'])
             else:
                 print('Specific .mp4 not found. Moving file named ' + File + ' to default folder.')
-                shutil.move(WatchedFolder + '/' + File, "E:/Game install files/Valve/temporary")
-                print('Copied to Folder')
+                FileMove(File, FileDef['.mp4'])
+        # .png Checker
         elif File.endswith(".png"):
             print("PNG Found")
+            FileMove(File, FileDef['.png'])
+        # .jpg Checker
         elif File.endswith(".jpg"):
             print("jpg Found")
+            FileMove(File, FileDef['.jpg'])
+        # Installer/.exe Checker
+        elif File.endswith(".exe"):
+            FileDelete(File, '.exe')
 
 
 MoveByName()
